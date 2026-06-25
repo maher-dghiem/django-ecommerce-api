05d5f41a3b5bb48fef4024762162886354db085d
maher-dghiem <maherda5.9@gmail.com>
2026-06-25 05:55:19 +0300

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -6,3 +6,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "price", "stock")
     list_filter = ("price",)
     search_fields = ("name",)
+    readonly_fields = ("created_at", "updated_at")
+    list_editable = ("price", "stock")
+
